bf2942d1de19c6021caabf6cda918560b8211130
GrupoSequencial <33881813+ericaolopess@users.noreply.github.com>
2022-09-19 20:34:09 -0300

    teste

---
+++ BubbleSort/src/BubbleAnimais/Animal.java
@@ -0,0 +1,33 @@
+package BubbleAnimais;
+
+/**
+ *
+ * @author erica
+ */
+public class Animal {
+  
+  private String especie;
+  private String nome;
+
+  public Animal(String especie, String nome) {
+    this.especie = especie;
+    this.nome = nome;
+  }
+
+  public String getEspecie() {
+    return especie;
+  }
+
+  public void setEspecie(String especie) {
+    this.especie = especie;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+    
+}
--- BubbleSort/src/bubblesort/BubbleSort.java
+++ BubbleSort/src/bubblesort/BubbleSort.java
@@ -1,14 +1,23 @@
 
 package bubblesort;
 
+import javax.swing.JOptionPane;
+
 public class BubbleSort {
   public static void main(String args[]) {
-    int[] v = {21,5,13,17,22,0,4};
+      int tamanho;
+      tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho do vetor:"));
+      
+     int[] v = new int[tamanho];
+    
+    for (int x=0; x < v.length; x++)
+        v[x] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor:"));
+    
     
      for(int num : v) {
       System.out.print(num + " ");
     }
-     System.out.println(" ");
+     System.out.println("");
      
      
     BubbleSort bs = new BubbleSort();
@@ -16,14 +25,14 @@ public class BubbleSort {
     
     for(int num : v) {
       System.out.print(num + " ");
-    }
+   }
   }
   
   public void ordenar(int[] v) {
     for(int i = 0; i < v.length - 1; i++) {
       //  System.out.println("i = " + i);
       for(int j = 0; j < v.length - 1 - i ; j++) {
-              if(v[j] > v[j + 1]) {
+              if(v[j] < v[j + 1]) {
             
           int aux = v[j];
           v[j] = v[j + 1];
